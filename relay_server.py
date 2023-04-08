5ede0a298595cecef7bdd8514f821134f63a44da
Milin Kodnongbua <mil.millin@hotmail.com>
2023-04-07 22:52:54 -0700

    add pwd

--- relay_server.py
+++ relay_server.py
@@ -2,25 +2,43 @@ from http.server import BaseHTTPRequestHandler, HTTPServer
 from urllib.parse import urlparse, parse_qs
 from subprocess import STDOUT, check_output
 import argparse
+import os
 
-class handler(BaseHTTPRequestHandler):
+class Handler(BaseHTTPRequestHandler):
+  def __init__(self, request, client_address, server):
+    self.pwd = os.getenv('MILUAI_PWD')
+    super().__init__(request, client_address, server)
+
   def do_GET(self):
     query_components = parse_qs(urlparse(self.path).query)
     try:
+      print(query_components)
       cmd = query_components["cmd"][0]
+      pwd = query_components["pwd"][0]
+      print(pwd, self.pwd)
+      if pwd != self.pwd:
+        raise Exception("wrong password")
       res = check_output(cmd, stderr=STDOUT, timeout=1, shell=True)
       self.send_response(200)
       self.send_header('Content-type','text/plain')
       self.end_headers()
       self.wfile.write(res)
-    except Exception:
+    except Exception as e:
+      print(e)
       self.send_response(403)
+      self.send_header('Content-type','text/plain')
+      self.end_headers()
+      self.wfile.write(bytes("denied", "utf-8"))
 
 def main():
   parser = argparse.ArgumentParser()
   parser.add_argument("-p", "--port", default=8000, type=int)
   args = parser.parse_args()
-  with HTTPServer(('', args.port), handler) as server:
+  pwd = os.getenv("MILUAI_PWD")
+  if pwd is None:
+    print("please define MILUAI_PWD environment variable")
+    return
+  with HTTPServer(('', args.port), Handler) as server:
     print(f"listening on port {args.port}")
     server.serve_forever()
 
